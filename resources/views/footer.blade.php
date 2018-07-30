6ad12e384874179cc1487cdb9eefa4a476a52726
yiiwen <18825179513@163.com>
2018-07-30 19:33:40 +0800

    右边导航

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -2,8 +2,8 @@
                 <div class="container">
                 <div class="row">
                     <div class="col-md-10">
-                    <p>联系地址：广东省广州市南沙区丰泽东路106号  联系电话：15818848247</p>
-                    <p>Copyright © 2018-2030,www.itromaine.com,All rights reserved  版权所有 © 您的网站名称</p>
+                    <p>联系地址：广东省广州市南沙区丰泽东路106号</p>
+                    <p>Copyright © 2018-2030,www.itromaine.com,All rights reserved  版权所有 © 广州油麦菜信息科技有限公司</p>
                     <p>备案号：粤ICP备18091280号</p>
                     </div>
                     <div class="col-md-2">
@@ -28,6 +28,12 @@
                     <img src="/images/right/top.png" alt="">
                 </li>
             </ul>
+            <div class="qrcode">
+                <img src="/images/qrcode.jpg" alt="">
+            </div>
+            <div class="search">
+                <input type="text" >
+            </div>
         </div>
         <!-- jQuery (Bootstrap 的所有 JavaScript 插件都依赖 jQuery，所以必须放在前边) -->
         <script src="https://cdn.bootcss.com/jquery/1.12.4/jquery.min.js"></script>
@@ -44,7 +50,32 @@
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
             },
-        })
+        });
+        window.onscroll = function(){
+            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                $(".right-nav li:last-child").show(10);
+            } else {
+                $(".right-nav li:last-child").hide(10);
+            }
+        };
+        $(".right-nav li:last-child").click(function(){
+            $('body,html').animate({scrollTop:0},200);
+        });
+        $(".right-nav li:nth-child(2)").hover(function(){
+            $(".qrcode").show(100);
+        });
+        $(".right-nav li:nth-child(2)").mouseleave(function(){
+            $(".qrcode").hide(100);
+        });
+        $(".right-nav li:nth-child(1)").hover(function(){
+            $(".search").show(100);
+        });
+        $(".search").hover(function(){
+            $(".search").show(100);
+        });
+        $(".right-nav li:nth-child(1)").mouseleave(function(){
+            $(".search").hide(100);
+        });
         </script>
     </body>
 </html>
