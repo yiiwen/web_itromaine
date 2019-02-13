944d385bb031c31c06049ce835e57b5c057d3a3a
wangweiwen <wen18826478504@163.com>
2019-02-13 13:44:15 +0800

    修改网址底部信息

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,15 +1,51 @@
         <div class="bottom-info">
                 <div class="container">
-                <div class="row">
-                    <div class="col-md-10">
-                    <p>联系地址：广东省广州市南沙区丰泽东路106号</p>
-                    <p>Copyright © 2018-2030,www.itromaine.com,All rights reserved  版权所有 © 广州油麦菜信息科技有限公司</p>
-                    <p>备案号：粤ICP备18091280号</p>
-                    </div>
-                    <div class="col-md-2">
-                    <img src="/images/qrcode.jpg" class="qr_code" alt="">
+                    <div class="row bottom-nav">
+                        <div class="col-md-4">
+                            <img src="/images/logo2.png" style="width:300px;" alt="">
+                            <h4 style="color:#FFF;margin-top:0;margin-left:104px;">您身边的高端定制专家</h4>
+                        </div>
+                        <div class="col-md-2">
+                            <dl>
+                                <dt>服务内容</dt>
+                                <dd><a href="">高端网站建设</a></dd>
+                                <dd><a href="">小程序定制开发</a></dd>
+                                <dd><a href="">软件设计与开发</a></dd>
+                                <dd><a href="">移动应用与开发</a></dd>
+                            </dl>
+                        </div>
+                        <div class="col-md-2">
+                            <dl>
+                                <dt>案例展示</dt>
+                                <dd><a href="">广州电视台</a></dd>
+                                <dd><a href="">心声社区</a></dd>
+                                <dd><a href="">cc女神</a></dd>
+                                <dd><a href="">更多展示</a></dd>
+                            </dl>
+                        </div>
+                        <div class="col-md-2">
+                            <dl>
+                                <dt>关于</dt>
+                                <dd><a href="">关于我们</a></dd>
+                                <dd><a href="">公司动态</a></dd>
+                                <dd><a href="">留言板</a></dd>
+                                <dd><a href="">网站地图</a></dd>
+                            </dl>
+                        </div>
+                        <div class="col-md-2">
+                            <img src="/images/qrcode.jpg" class="qr_code" alt="">
+                        </div>
                     </div>
                 </div>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-10">
+                            
+                            <p>联系电话：15818848247&nbsp;&nbsp;&nbsp;&nbsp;13668984401&nbsp;&nbsp;&nbsp;&nbsp;联系邮箱：youmaicai@163.com</p>
+                            <p>联系地址：广东省广州市南沙区丰泽东路106号&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;备案号：粤ICP备18091280号</p>
+                            <p>Copyright © 2018-2030,www.itromaine.com,All rights reserved  版权所有 © 广州油麦菜信息科技有限公司</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
