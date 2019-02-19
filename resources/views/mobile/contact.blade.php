825d53a07d70be46b3d94dee6a4a0142b4b1fd67
wangweiwen <wen18826478504@163.com>
2019-02-19 17:27:54 +0800

    修改移动端和PC端介绍

--- resources/views/mobile/contact.blade.php
+++ resources/views/mobile/contact.blade.php
@@ -11,29 +11,29 @@
             </div>
         </div>
         <div class="contact-content">
-            <h3 class="contact-phone">咨询 Tel：<span style="white-space:nowrap;">+86 020 8888-8888</span></h3>
+            <h3 class="contact-phone">咨询 Tel：<span style="white-space:nowrap;">+86 158 1884 8247</span></h3>
             <p>马上致电咨询即可免费获得需求分析、报价、策划、设计、技术等顾问服务，
-                我们相信良好的沟通是发挥团队作用最好的开端。如果您已经准备好需求文档可发送致电子邮箱 
+                我们相信良好的沟通是发挥团队作用最好的开端。如果您已经准备好需求文档可发送致电子邮箱
                 <a href="mailto:youmaicai168@163.com">youmaicai168@163.com</a>，我们一定以最快的时间回复您。
             </p>
             <p style="font-size:1.2em;font-weight:bold;">业务：企业网站设计与制作&amp;商城平台开发&amp;腾迅云&amp;阿里云&amp;SEO搜索引挚优化</p>
             <em>广州油麦菜信息科技有限公司</em>
             <p>Guangzhou romaine information technology LTD.CO</p>
             <p>
-                <span class="locations" data-map-address="深圳设计产业园金栋二楼213-218"> 
+                <span class="locations" data-map-address="深圳设计产业园金栋二楼213-218">
                 <span class="location-details">地址：广州南沙区丰泽路106号自编一号楼</span> </span><br>
             </p>
             <p>
-                业务咨询（Tel）：<span style="white-space: nowrap;">+86 020 88888888</span><br>
+                业务咨询（Tel）：<span style="white-space: nowrap;">+86 158 1884 8247</span><br>
                 业务咨询（QQ）：<a target="_blank" href="http://wpa.qq.com/msgrd?v=3&amp;uin=2115634603&amp;site=qq&amp;menu=yes" textvalue="2115634603">2115634603</a><br>
-                客服电话（Tel）：<span style="white-space: nowrap;">+86 020 88888888</span><br>
+                客服电话（Tel）：<span style="white-space: nowrap;">+86 136 6898 4401</span><br>
                 客户服务（QQ）：<a target="_blank" href="http://wpa.qq.com/msgrd?v=3&amp;uin=2070393&amp;site=qq&amp;menu=yes" textvalue="2070393">2070393</a>
             </p>
             <blockquote style="margin-left:0px;border-left:1px solid #CCC;padding-left:10px;">
                 <em>温馨提示：</em>
                 <p>1、您致电咨询时，我们将记录您的信息，然后由设计顾问与您沟通；</p>
                 <p>2、以确保报价的准确、合理，我们会向您索取尽量详细的项目需求</p>
-                <p>3、本公司未授权任何代理或联盟单位，请勿上当受骗。 </p>   
+                <p>3、本公司未授权任何代理或联盟单位，请勿上当受骗。 </p>
             </blockquote>
         </div>
 
