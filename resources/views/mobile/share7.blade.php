c8978278ea344ba0730f82a30698c93c2cef1572
rjomg <1241718398@qq.com>
2019-02-20 15:39:11 +0800

    移动端 知识

---
+++ resources/views/mobile/share7.blade.php
@@ -0,0 +1,27 @@
+@include('/mobile/header')
+@include('/mobile/nav')
+<div class="container">
+  <div id="pagePath">首页 > 建站知识</div>
+  <!-- 中部导航栏 -->
+  <div class="sharepage">
+    <div class="shareTop">
+      <h2>揭示了2020年的非正式PANTONE色彩</h2>
+      <span class="shareAuthor">By:itromane</span>-
+      <span class="shareTime">2019-02-18 12:40:48</span>
+    </div>
+
+
+      <div class="shareContent">
+        {{--<p><img src="/images/share/15486616895776.jpg" alt="" /></p>--}}
+        <p>每年12月，色彩分类公司Pantone都会指定一种颜色作为未来12个月的色调。该法案旨在围绕环境、性别平等和时事等信息创造积极的氛围。但是2019年的颜色在一些地方引起了轩然大波。2019年的官方颜色被命名为Living Coral，它是蓝色水下红珊瑚的理想色调。</p>
+        <p>根据Jack和huei（一家墨尔本的设计机构）的说法，争议在于潘通的选择掩盖了气候变化对珊瑚造成的破坏。该机构由Jack Railton-Woodcock和Huei Yin Wong运营，他们特别关注澳大利亚本土珊瑚的消失，尤其是大堡礁的消失。两人形容潘通的选择“音盲，完全不负责任”。</p>
+        <p>不满足于简单地抵制Pantone 2019年的选择，这对搭档将明年的建议颜色颠覆这一决定：</p>
+        <p>这是我们所有人的责任，创造性的或其他的，找到创造性的方法来解决大问题，现在人类面临的问题没有比气候变化更大的了。</p>
+        <p>Jack和Huei在他们的潘通样本中找到了F0F6F7，这是一种非常浅蓝的颜色，与珊瑚骨骼茎的颜色相匹配。然后，他们给这种颜色取了个绰号，叫“珊瑚白”（bleblecoral），并盗用潘通的品牌，为明年的选择推荐这种颜色：</p>
+        <p>这是我们深切关注的一个问题，我们认为创意产业有机会将这个全球性问题从海洋深处带到屏幕表面。</p>
+        <p>Pantone在2018年底的声明被许多人视为一个狭隘的决定，Jack和Huei的游击式设计抗议是设计师们利用他们的手艺来解决严重的社会问题这一更广泛趋势的一部分。Pantone不太可能接受这个建议，但一个道德的、政治化的设计世界的趋势将持续到2020年。</p>
+        <p><img src="/images/share/15483014288983.jpg"></p>
+      </div>
+    </div>
+</div>
+@include('/mobile/footer')
